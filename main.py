4ec61b1ee25a1bd77fe8cfa7273806446cec0abf
Shakeeb Shaikh <eren.85yeager@gmail.com>
2025-10-22 19:52:32 +0530

    init Function is impelmented
    This version now contains the Initializing the Git in our working Directory

--- main.py
+++ main.py
@@ -1,4 +1,46 @@
 import argparse
+import sys
+from pathlib import Path
+import json
+
+class Repository:
+    def __init__(self,path="." ):
+        self.path = Path(path).resolve()
+        self.git_dir = self.path / ".pygit"
+        
+        #.git/ojects
+        self.objects_dir = self.git_dir / "objects"
+        
+        #.git/refs
+        self.ref_dir = self.git_dir / "refs"
+        self.heads_dir = self.ref_dir / "heads"
+        
+        #.Head File
+        self.head_file = self.git_dir / "HEAD"
+        
+        #.git/index
+        self.index_file = self.git_dir / "index"
+        
+    def init(self) -> bool:
+        
+        if self.git_dir.exists():
+            print("Repository already exists.")
+            return False
+        
+        #create directories
+        self.git_dir.mkdir()
+        self.objects_dir.mkdir()
+        self.ref_dir.mkdir()
+        self.heads_dir.mkdir()
+        
+        #create inital HEAD pointing to a branch
+        self.head_file.write_text("ref: refs/heads/master\n")
+        
+        self.index_file.write_text(json.dumps({}, indent=4))
+        
+        print(f"Initialized empty Git repository in {self.git_dir}")
+        return True
+    
 
 def main():
     parser = argparse.ArgumentParser(
@@ -12,7 +54,22 @@ def main():
     
     args = parser.parse_args()
     
-    print(args)
+    if not args.command:
+        parser.print_help()
+        return
+    
+    try:
+        if args.command == "init":
+            repo = Repository()
+            if not repo.init():
+                print("Repository already exists.")
+                return
+            # print("Initializing a new git repository...")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        sys.exit(1)
+    
+    # print(args)
 
 main()
 
