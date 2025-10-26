6038358505f48f9479b8fb75b2fca856e90c6638
Shakeeb Shaikh <eren.85yeager@gmail.com>
2025-10-26 10:49:47 +0530

    Implement GitObject class with serialization methods

--- git.py
+++ git.py
@@ -8,3 +8,26 @@ import time
 from typing import Dict, List, Optional, Tuple, Set
 import zlib
 import difflib
+
+
+class GitObject:
+    def __init__(self, obj_type: str, content: bytes):
+        self.type = obj_type
+        self.content = content
+
+    def hash(self) -> str:
+        header = f"{self.type} {len(self.content)}\0".encode()
+        return hashlib.sha1(header + self.content).hexdigest()
+
+    def serialize(self) -> bytes:
+        header = f"{self.type} {len(self.content)}\0".encode()
+        return zlib.compress(header + self.content)
+
+    @classmethod
+    def deserialize(cls, data: bytes) -> GitObject:
+        decompressed = zlib.decompress(data)
+        null_idx = decompressed.find(b"\0")
+        header = decompressed[:null_idx].decode()
+        content = decompressed[null_idx + 1 :]
+        obj_type, _ = header.split(" ")
+        return cls(obj_type, content)
